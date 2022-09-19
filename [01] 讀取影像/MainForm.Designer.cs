96f21fc2b0c15ae4af3a72883d89bbd5199ba912
Nil0516 <39086273+Nil0516@users.noreply.github.com>
2022-09-19 17:32:59 +0800

    Add files via upload

---
+++ [01] 讀取影像/MainForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace MyEmgucv
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxImagePath = new System.Windows.Forms.TextBox();
+            this.buttonGetImagePath = new System.Windows.Forms.Button();
+            this.pictureBoxDisplayImage = new System.Windows.Forms.PictureBox();
+            this.button2GrayImage = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplayImage)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxImagePath
+            // 
+            this.textBoxImagePath.Location = new System.Drawing.Point(12, 12);
+            this.textBoxImagePath.Name = "textBoxImagePath";
+            this.textBoxImagePath.Size = new System.Drawing.Size(651, 33);
+            this.textBoxImagePath.TabIndex = 0;
+            // 
+            // buttonGetImagePath
+            // 
+            this.buttonGetImagePath.Location = new System.Drawing.Point(669, 12);
+            this.buttonGetImagePath.Name = "buttonGetImagePath";
+            this.buttonGetImagePath.Size = new System.Drawing.Size(155, 33);
+            this.buttonGetImagePath.TabIndex = 1;
+            this.buttonGetImagePath.Text = "讀取影像";
+            this.buttonGetImagePath.UseVisualStyleBackColor = true;
+            this.buttonGetImagePath.Click += new System.EventHandler(this.buttonGetImagePath_Click);
+            // 
+            // pictureBoxDisplayImage
+            // 
+            this.pictureBoxDisplayImage.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBoxDisplayImage.Location = new System.Drawing.Point(12, 51);
+            this.pictureBoxDisplayImage.Name = "pictureBoxDisplayImage";
+            this.pictureBoxDisplayImage.Size = new System.Drawing.Size(812, 694);
+            this.pictureBoxDisplayImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxDisplayImage.TabIndex = 2;
+            this.pictureBoxDisplayImage.TabStop = false;
+            // 
+            // button2GrayImage
+            // 
+            this.button2GrayImage.Location = new System.Drawing.Point(6, 32);
+            this.button2GrayImage.Name = "button2GrayImage";
+            this.button2GrayImage.Size = new System.Drawing.Size(137, 47);
+            this.button2GrayImage.TabIndex = 3;
+            this.button2GrayImage.Text = "影像灰階";
+            this.button2GrayImage.UseVisualStyleBackColor = true;
+            this.button2GrayImage.Click += new System.EventHandler(this.button2GrayImage_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2GrayImage);
+            this.groupBox1.Location = new System.Drawing.Point(830, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(497, 693);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "影像處理";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 756);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBoxDisplayImage);
+            this.Controls.Add(this.buttonGetImagePath);
+            this.Controls.Add(this.textBoxImagePath);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplayImage)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxImagePath;
+        private System.Windows.Forms.Button buttonGetImagePath;
+        private System.Windows.Forms.PictureBox pictureBoxDisplayImage;
+        private System.Windows.Forms.Button button2GrayImage;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ [01] 讀取影像/MainForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace MyEmgucv
+{
+    public partial class MainForm : Form
+    {
+        string m_imagePath;
+        Image<Bgr, Byte> m_image;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGetImagePath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            // 顯示預設路徑。
+            ofd.InitialDirectory = "./";
+            // 設定檔案儲存類型範例為.bmp若要增加其他類型可使用{名稱}|.{類型}。
+            ofd.Filter = "Bitmap Files|*.bmp";
+            ofd.Title = "選擇影像檔案";
+            // 紀錄最後視窗路徑位置。
+            ofd.RestoreDirectory = true;
+            // 顯示視窗，當按下確定(OK)時進入。
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // 保存使用者選擇檔案的路徑位置
+                m_imagePath = ofd.FileName;
+                textBoxImagePath.Text = m_imagePath;
+                // 讀取影像並顯示。
+                DisplayImage(m_imagePath);
+            }
+        }
+
+        private void DisplayImage(string path)
+        {
+            try
+            {
+                //Bitmap bitmap = new Bitmap(m_imagePath);
+                m_image = new Image<Bgr, byte>(path);
+                pictureBoxDisplayImage.Image = (Bitmap)m_image.ToBitmap().Clone();
+                pictureBoxDisplayImage.Invalidate();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void button2GrayImage_Click(object sender, EventArgs e)
+        {
+            if (m_image == null) return;
+
+            Image<Gray, Byte> grayImage = new Image<Gray, Byte>(m_image.Width, m_image.Height, new Gray(255));
+            //
+            grayImage = m_image.Convert<Gray, Byte>();
+            pictureBoxDisplayImage.Image = (Bitmap)grayImage.ToBitmap().Clone();
+            pictureBoxDisplayImage.Invalidate();
+        }
+    }
+}
